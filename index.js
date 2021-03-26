2bbfd9dcef541a41d5824409abefaaa9aec483d9
Lina Deen <linadeen@Linas-MacBook-Pro.local>
2021-03-25 19:35:13 -0700

    commit

---
+++ index.js
@@ -0,0 +1,231 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const mysql = require('mysql');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: 'Unicorn!',
+    resave: false,
+    saveUnitialized: true,
+  })
+);
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+// Movie store
+app.get('/displayMovie', async (req, res) => {
+  res.render('displayMovie');
+});
+
+// Shopping cart
+app.get('/cart', (req, res) => {
+  res.render('cart');
+});
+
+// Login
+app.post('/', async (req, res) => {
+  let username = req.body.username;
+  let userPassword = req.body.password;
+
+  let result = await checkUsername(username);
+
+  let password = '$2a$10$LH07sO0xioh7EfukesR3Yeh2a9j7VbKJijZMS6tY6QYgXXzOd58RG'; //secret hashed bycrpt password
+
+  let passwordMatch = await bcrypt.compare(userPassword, password);
+  passwordMatch = true;
+
+  req.session.authenticated = false;
+  if (passwordMatch) {
+    console.log(`User: ${username} logged in`);
+    req.session.authenticated = true;
+    res.render('displayMovie');
+  } else {
+    res.render('login', { loginError: true });
+  }
+});
+
+// Account info
+app.get('/account', isAuthenticated, (req, res) => {
+  if (req.session.authenticated) {
+    res.render('account');
+  } else {
+    res.redirect('/');
+  }
+});
+
+// view movies
+app.get('/viewMovies', async (req, res) => {
+  var conn = createDBConnection();
+
+  let sql = 'SELECT idmovie,title,producer FROM movie ORDER BY title';
+  // let rows = await executeSQL(sql);
+
+  conn.connect(function (err) {
+    if (err) throw err;
+
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      res.render('viewMovies', { movies: result });
+    });
+  });
+});
+
+// Deletes a movie  passed a movie ID if the user is authenticated.
+app.get('/deleteMovie', isAuthenticated, (req, res) => {
+  var conn = createDBConnection();
+
+  var sql = 'DELETE FROM movie where idmovie = ?';
+  var sqlParams = [req.query.idmovie];
+
+  conn.connect(function (err) {
+    if (err) throw err;
+
+    conn.query(sql, sqlParams, (err, result) => {
+      if (err) throw err;
+    });
+  });
+  res.send('movie is deleted !');
+});
+
+// Add a movie
+app.get('/addMovie', isAuthenticated, function (req, res) {
+  var conn = createDBConnection();
+
+  var sql = 'INSERT INTO movie (title,producer,rating) values (?,?,?)';
+
+  var title = parseFloat(req.query.title);
+  var producer = parseInt(req.query.producer);
+  var rating = parseInt(req.query.rating);
+
+  var sqlParams = [req.query.title, producer, rating];
+
+  conn.connect(function (err) {
+    if (err) throw err;
+
+    conn.query(sql, sqlParams, function (err, result) {
+      if (err) throw err;
+    });
+  });
+  res.send('movie is added!');
+});
+
+// Logout from session
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+// Get all movies
+app.get('/getAllMovies', (req, res) => {
+  var conn = createDBConnection();
+  //var sql = "SELECT * from movie order by title"
+  var sql = 'SELECT title, producer, rating  FROM movie JOIN movie  ORDER BY b.title';
+  conn.connect(function (err) {
+    if (err) throw err;
+    conn.query(sql, function (err, results) {
+      if (err) throw err;
+      res.send(results);
+    });
+  });
+});
+
+// Search movies by Title
+app.get('/getproducer', (req, res) => {
+  var conn = createDBConnection();
+  var sql = 'SELECT title, producerFROM movie  ORDER BY producer';
+  var sqlParams = [req.query.title];
+  conn.connect(function (err) {
+    if (err) throw err;
+    conn.query(sql, sqlParams, function (err, results) {
+      if (err) throw err;
+      console.log(results);
+      res.send(results);
+    });
+  });
+});
+
+// Search movie by producer
+app.get('/getrating', (req, res) => {
+  var conn = createDBConnection();
+  var sql = 'SELECT title, rating from movie GROUP BY idmovie ORDER BY b.title';
+  var sqlParams = [req.query.title, req.query.rating];
+  conn.connect(function (err) {
+    if (err) throw err;
+    conn.query(sql, sqlParams, function (err, results) {
+      if (err) throw err;
+      console.log(results);
+      res.send(results);
+    });
+  });
+});
+
+// middleware function for all pages wew need it to be password protected/middleware function
+function isAuthenticated(req, res, next) {
+  if (!req.session.authenticated) {
+    res.render('login');
+  } else {
+    next(); //excute the code we have in the route
+  }
+}
+
+function createDBConnection() {
+  var conn = mysql.createConnection({
+    host: "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ql1x71wowj14etor",
+    password: "uvtjlhdytgy67dm3",
+    database: "abxsz67grf7sjdso",
+  });
+  return conn;
+} //create DB connection
+
+function checkUsername(username) {
+  let sql = "SELECT * FROM users WHERE username = ?";
+  return new Promise(function (resolve, reject) {
+    conn.query(sql, [username], function (err, rows, fields) {
+      if (err) throw err;
+      resolve(rows);
+    });
+  });
+}
+
+function checkPassword(password, hashedPW) {
+  return new Promise(function (resolve, reject) {
+    bcrypt.compare(password, hashedPW, function (err, result) {
+      console.log("Password: " + password);
+      console.log("hashedPW: " + hashedPW);
+      console.log("Result: " + result);
+      resolve(result);
+    });
+  });
+}
+
+function isAuthenticated(req, res, next) {
+  if (!req.session.authenticated) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+}
+
+const PORT = process.env.Port ||3001;
+app.listen(PORT, () => {
+  // Creating connection
+  // 
+  
+});
+
+// let port =process.env.PORT || 3001;
+
+// // //Heroku Listener
+// app.listen(port,()=>{
+//     console.log(`Express Server is Running...${port}`);
+
+// });
